f48c6cd67b8d91ba860542a70c2f073ca849cd71
JacCars <carstensjacques3@gmail.com>
2020-12-14 19:31:03 +0200

    more

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -66,13 +66,13 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[1:300])
-        movie_2 = st.selectbox('Second Option',title_list[550:555])
-        movie_3 = st.selectbox('Third Option',title_list[311:540])
+        #movie_1 = st.selectbox('Fisrt Option',title_list[1:300])
+        #movie_2 = st.selectbox('Second Option',title_list[550:555])
+        #movie_3 = st.selectbox('Third Option',title_list[311:540])
 
-        # movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-        # movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-        # movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
+        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
         fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
